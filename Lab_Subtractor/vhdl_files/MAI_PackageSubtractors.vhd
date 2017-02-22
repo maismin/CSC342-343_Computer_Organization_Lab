124ea791e068977c85fbdd34caacafaded184727
Si Mai <maismin@gmail.com>
2017-02-22 00:20:55 -0500

    fixed and verified that the test bench for the four bit subtractor works

--- Lab_Subtractor/vhdl_files/MAI_PackageSubtractors.vhd
+++ Lab_Subtractor/vhdl_files/MAI_PackageSubtractors.vhd
@@ -13,5 +13,15 @@ package MAI_PackageSubtractors is
           Z    : out std_logic);
   end component;
   
+  component mai_eight_bit_add_sub
+  port ( X, Y : in std_logic_vector(7 downto 0);
+         SEL  : in std_logic;   -- 1 for subtract, 0 for add
+         SUM  : out std_logic_vector(7 downto 0);
+         Cout : out std_logic;
+         V    : out std_logic;
+         N    : out std_logic;
+         Z    : out std_logic);
+  end component; 
+  
 end package MAI_PackageSubtractors;
 
